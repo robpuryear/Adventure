9dbf6651870cba125cd821c24d45f420c006b04c
Rob Puryear <sdpuryea@us.ibm.com>
2020-02-09 13:50:47 -0500

    First

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadMissionScene()
+    {
+        SceneManager.LoadScene("MissionHome");
+    }
+
+    public void LoadEditCharacterScene()
+    {
+        SceneManager.LoadScene("EditCharacter");
+    }
+
+    public void LoadPartyScene()
+    {
+        SceneManager.LoadScene("PartyScene");
+    }
+
+    public void LoadStoryScene()
+    {
+        SceneManager.LoadScene("StoryScene");
+    }
+}
