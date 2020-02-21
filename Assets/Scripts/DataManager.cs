2c1e16dc211c9151667d8d74c0941489bceccb2b
Rob Puryear <robpuryear@yahoo.com>
2020-02-21 16:08:46 -0500

    Fixing DataManager from last commit
    I mistakenly removed / changed some functions in DataManager I thought I didn't need.

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -6,6 +6,9 @@ using SimpleJSON;
 
 public class DataManager : MonoBehaviour
 {
+    private string file = "player1.txt";
+    public PlayerData data;
+
     public JSONState state;
     public JSONNode json;
 
@@ -28,50 +31,50 @@ public class DataManager : MonoBehaviour
         json = JSON.Parse(jsonTextFile.ToString());
     }
 
-    //public void Save()
-    //{
-    //    string json = JsonUtility.ToJson(state);
-    //    WriteToFile(file, json);
-    //}
+    public void Save()
+    {
+        string json = JsonUtility.ToJson(data);
+        WriteToFile(file, json);
+    }
 
-    //public void Load()
-    //{
-    //    state = new JSONState();
-    //    string json = ReadFromFile(file);
-    //    JsonUtility.FromJsonOverwrite(json, state);
-    //}
+    public void Load()
+    {
+        data = new PlayerData();
+        string json = ReadFromFile(file);
+        JsonUtility.FromJsonOverwrite(json, data);
+    }
 
-    //private void WriteToFile(string fileName, string json)
-    //{
-    //    string path = GetFilePath(fileName);
-    //    FileStream fileStream = new FileStream(path, FileMode.Create);
+    private void WriteToFile(string fileName, string json)
+    {
+        string path = GetFilePath(fileName);
+        FileStream fileStream = new FileStream(path, FileMode.Create);
 
-    //    using (StreamWriter writer = new StreamWriter(fileStream))
-    //    {
-    //        writer.Write(json);
-    //    }
-    //}
+        using (StreamWriter writer = new StreamWriter(fileStream))
+        {
+            writer.Write(json);
+        }
+    }
 
-    //private string ReadFromFile(string fileName)
-    //{
-    //    string path = GetFilePath(fileName);
-    //    if (File.Exists(path))
-    //    {
-    //        using (StreamReader reader = new StreamReader(path))
-    //        {
-    //            string json = reader.ReadToEnd();
-    //            return json;
-    //        }
-    //    }
-    //    else
-    //        Debug.LogWarning("File not found!");
+    private string ReadFromFile(string fileName)
+    {
+        string path = GetFilePath(fileName);
+        if (File.Exists(path))
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string json = reader.ReadToEnd();
+                return json;
+            }
+        }
+        else
+            Debug.LogWarning("File not found!");
 
-    //    return "";
-    //}
+        return "";
+    }
 
-    //private string GetFilePath(string fileName)
-    //{
-    //    return Application.persistentDataPath + "/" + fileName;
-    //}
+    private string GetFilePath(string fileName)
+    {
+        return Application.persistentDataPath + "/" + fileName;
+    }
 
 }
